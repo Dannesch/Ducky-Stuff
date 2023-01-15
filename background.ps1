3ca058ee499dd3cfa6d0b794ba4ed49bafe5ffa3
Daniel Schembri <35847188+Dannesch@users.noreply.github.com>
2023-01-15 15:52:37 +0100

    Update and rename background to background.ps1

--- background.ps1
+++ background.ps1
@@ -1,2 +1,3 @@
+Invoke-WebRequest "https://filesamples.com/samples/image/jpg/sample_1920%C3%971280.jpg" -OutFile C:\ProgramData\cool.jpg
 Function Set-WallPaper($Value){Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $value | rundll32.exe user32.dll, UpdatePerUserSystemParameters}
-Set-WallPaper -Value "%userprofile%\1dv8ac.jpg"
+Set-WallPaper -Value "C:\ProgramData\cool.jpg"
